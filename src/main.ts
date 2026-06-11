9987133c878fc5e9e27bd52c7e2cb358cb12f33a
spldg <capybara322@gmail.com>
2026-06-11 14:23:58 +0400

    promisify game asset loading

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,51 @@
 import * as PIXI from 'pixi.js'
 import { GameScene } from './GameScene'
+import { loadSounds } from './sound'
 
-const app = new PIXI.Application({
-    resizeTo: window,
-    backgroundColor: 0xd3d3d3,
-    antialias: true
-})
-const container = document.getElementById('pixi-container')
-if (!container) {
-    throw new Error('container not found')
-}
-container.appendChild(app.view)
-async function bootstrap(): Promise<void> {
-    await document.fonts.load('38px "Press Start 2P"')
-    await document.fonts.load('20px "Silkscreen"')
+async function loadFonts(): Promise<void> {
+    await Promise.all([
+        document.fonts.load('38px "Press Start 2P"'),
+        document.fonts.load('20px "Silkscreen"'),
+    ])
+
     await document.fonts.ready
+}
+
+async function bootstrap(): Promise<void> {
+    await Promise.all([
+        loadFonts(),
+        loadSounds(),
+    ])
+
+    const app = new PIXI.Application({
+        resizeTo: window,
+        backgroundColor: 0xd3d3d3,
+        antialias: true,
+    })
+
+    const container = document.getElementById('pixi-container')
+    if (!container) {
+        throw new Error('container not found')
+    }
+
+    container.appendChild(app.view)
 
     const gameScene = new GameScene()
     app.stage.addChild(gameScene)
+
     function resize(): void {
         const width = window.innerWidth
         const height = window.innerHeight
+
         gameScene.resize(width, height)
     }
+
     resize()
     window.addEventListener('resize', resize)
+
     app.ticker.add((delta: number) => {
         gameScene.update(delta)
     })
 }
+
 bootstrap()
--- src/sound.ts
+++ src/sound.ts
@@ -4,20 +4,54 @@ import lvlChangeFx from '../assets/sounds/nextLevel.mp3'
 import clearLinefx from '../assets/sounds/clearLineFx.mp3'
 import FX from '../assets/sounds/fx.mp3'
 
+function loadSound(sound: Howl): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (sound.state() === 'loaded') {
+            resolve()
+            return
+        }
+
+        sound.once('load', () => {
+            resolve()
+        })
+
+        sound.once('loaderror', (_, error) => {
+            reject(error)
+        })
+
+        sound.load()
+    })
+}
+
 export const music = new Howl({
-    src: bgMusic,
+    src: [bgMusic],
     loop: true,
     volume: 0.125,
+    preload: false,
 })
 
-export const levelChangeFx = new Howl ({
-    src: lvlChangeFx,
+export const levelChangeFx = new Howl({
+    src: [lvlChangeFx],
+    preload: false,
 })
 
-export const clearLineFx = new Howl ({
-    src: clearLinefx
+export const clearLineFx = new Howl({
+    src: [clearLinefx],
+    preload: false,
 })
 
-export const fx = new Howl ({
-    src: FX
+export const fx = new Howl({
+    src: [FX],
+    preload: false,
 })
+
+export function loadSounds(): Promise<void> {
+    return Promise
+        .all([
+            music,
+            levelChangeFx,
+            clearLineFx,
+            fx,
+        ].map(loadSound))
+        .then(() => undefined)
+}
